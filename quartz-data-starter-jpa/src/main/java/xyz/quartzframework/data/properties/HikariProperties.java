078b5b09ad219e9053654d1fd6b96f060f1d1267
Jorge <p3kkar@cryptovikings.gg>
2025-07-03 18:07:42 -0300

    feat(jpa): update properties classes with lombok and encapsulation (development)
    - Replaced @Value with @Getter in Hikari, Hibernate, and JPA properties
      classes to encapsulate fields.
    - Changed field access to private in all properties classes for better
      encapsulation.

--- quartz-data-starter-jpa/src/main/java/xyz/quartzframework/data/properties/HikariProperties.java
+++ quartz-data-starter-jpa/src/main/java/xyz/quartzframework/data/properties/HikariProperties.java
@@ -1,67 +1,67 @@
 package xyz.quartzframework.data.properties;
 
-import lombok.Value;
+import lombok.Getter;
 import xyz.quartzframework.core.bean.annotation.Injectable;
 import xyz.quartzframework.core.property.Property;
 
-@Value
+@Getter
 @Injectable
 public class HikariProperties {
 
     @Property("${quartz.data.hikari.minimum-idle:5}")
-    int hikariMinimumIdle;
+    private int hikariMinimumIdle;
 
     @Property("${quartz.data.hikari.maximum-pool-size:10}")
-    int hikariMaximumPoolSize;
+    private int hikariMaximumPoolSize;
 
     @Property("${quartz.data.hikari.connection-timeout:30000}")
-    long hikariConnectionTimeout;
+    private long hikariConnectionTimeout;
 
     @Property("${quartz.data.hikari.idle-timeout:600000}")
-    long hikariIdleTimeout;
+    private long hikariIdleTimeout;
 
     @Property("${quartz.data.hikari.max-lifetime:1800000}")
-    long hikariMaxLifetime;
+    private long hikariMaxLifetime;
 
     @Property("${quartz.data.hikari.keepalive-time:0}")
-    long hikariKeepaliveTime;
+    private long hikariKeepaliveTime;
 
     @Property("${quartz.data.hikari.leak-detection-threshold:0}")
-    long hikariLeakDetectionThreshold;
+    private long hikariLeakDetectionThreshold;
 
     @Property("${quartz.data.hikari.validation-timeout:5000}")
-    long hikariValidationTimeout;
+    private long hikariValidationTimeout;
 
     @Property("${quartz.data.hikari.connection-test-query:}")
-    String hikariConnectionTestQuery;
+    private String hikariConnectionTestQuery;
 
     @Property("${quartz.data.hikari.connection-init-sql:}")
-    String hikariConnectionInitSql;
+    private String hikariConnectionInitSql;
 
     @Property("${quartz.data.hikari.pool-name:QuartzHikariPool}")
-    String hikariPoolName;
+    private String hikariPoolName;
 
     @Property("${quartz.data.hikari.auto-commit:true}")
-    boolean hikariAutoCommit;
+    private boolean hikariAutoCommit;
 
     @Property("${quartz.data.hikari.read-only:false}")
-    boolean hikariReadOnly;
+    private boolean hikariReadOnly;
 
     @Property("${quartz.data.hikari.isolate-internal-queries:false}")
-    boolean hikariIsolateInternalQueries;
+    private boolean hikariIsolateInternalQueries;
 
     @Property("${quartz.data.hikari.register-mbeans:false}")
-    boolean hikariRegisterMbeans;
+    private boolean hikariRegisterMbeans;
 
     @Property("${quartz.data.hikari.allow-pool-suspension:false}")
-    boolean hikariAllowPoolSuspension;
+    private boolean hikariAllowPoolSuspension;
 
     @Property("${quartz.data.hikari.initialization-fail-timeout:1}")
-    long hikariInitializationFailTimeout;
+    private long hikariInitializationFailTimeout;
 
     @Property("${quartz.data.hikari.transaction-isolation:}")
-    String hikariTransactionIsolation;
+    private String hikariTransactionIsolation;
 
     @Property("${quartz.data.hikari.schema:}")
-    String hikariSchema;
+    private String hikariSchema;
 }
